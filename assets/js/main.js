175c06bcb4c1484df109d97686fdcb5da01eca87
aymantarek16 <aymantarek16@yahoo.com>
2022-04-07 18:13:36 +0200

    add scrollreval for animat when scroll in web page

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,3 +102,23 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin : 'top',
+    distance : '60px',
+    duration : '2500',
+    delay : 400,
+    // reset : true
+
+})
+
+sr.reveal(`.home__title , .popular__container,.features__img , .featured__filters`)
+sr.reveal(`.home__subtitle`, {delay : 500})
+sr.reveal(`.home__elec`, {delay : 600})
+sr.reveal(`.home__img`, {delay : 800})
+sr.reveal(`.home__car-data`, {delay : 800 , interval : 100 , origin : 'bottom'})
+sr.reveal(`.home__button`, {delay : 500 , origin : 'bottom'})
+sr.reveal(`.about__group , .offer__data`, { delay : 500,origin : 'left'})
+sr.reveal(`.about__data , offer__img`, { delay : 500 , origin : 'right'})
+sr.reveal(`.features__map`, { delay : 600 , origin : 'bottom'})
+sr.reveal(`.features__card`, {interval : 300 })
+sr.reveal(`.featured__card , logos__content , .footer__content`, {interval : 100 })
